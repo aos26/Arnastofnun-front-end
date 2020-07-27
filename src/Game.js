12c0534f4512c8087923eaf817a59c90b8c9cc68
aos26 <aos26@hi.is>
2020-07-27 23:19:08 +0000

    birt lista af orðum sem sótt er frá db

--- src/Game.js
+++ src/Game.js
@@ -1,36 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import Score from './components/layout/Score';
 
 export default function Word() {
 
-    const [word, setWord] = useState('');
+    const [words, setWords] = useState([]);
+    //const words = [];
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:5000/words/549')
-        .then(res => {
-            console.log(res)
-            setWord(res.data)
-        })
-        .catch(err => console.log(err));
+        axios.get('http://127.0.0.1:5000/words/5')
+            .then(res => {
+            const newWords = res.data.map(obj => obj);
+            setWords(newWords);
+            })
+            .catch(err => console.log(err));
+        
     }, []);
 
     const fetchWord = () => {
-        const id = Math.floor(Math.random() * 60000);
-        axios.get('http://127.0.0.1:5000/words/' + id)
-        .then(res => {
-            console.log(res)
-            setWord(res.data)
-        })
-        .catch(err => console.log(err));
+        axios.get('http://127.0.0.1:5000/words/5')
+            .then(res => {
+            const newWords = res.data.map(obj => obj);
+            setWords(newWords);
+            })
+            .catch(err => console.log(err));
     }
 
     return (
-        <div className="mt-4">
-            <div className="card">
-                <h5 className="card-header">Word</h5>
-                <div className="card-body">
-                    <p className="card-text">{word.texti}</p>
-                    <a className="btn btn-primary text-white" onClick={fetchWord}>Get another word</a>
+        <div className="container">
+            <Score />
+            <div className="mt-4">
+                <div className="card">
+                    <h5 className="card-header">Word</h5>
+                    <div className="card-body">
+                        {words.map(ord => (
+                            <li key={ord.id}>{ord.ord}</li>
+                        ))}
+                        <a className="btn btn-primary text-white" onClick={fetchWord}>Get another word</a>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/layout/Score.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Score() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                Rétt svör: 5
+                </div>
+                <div className="col">
+                Röng svör: 3
+                </div>
+                <div className="col">
+                Tími: 5:03
+                </div>
+            </div>
+        </div>
+    )
+}
