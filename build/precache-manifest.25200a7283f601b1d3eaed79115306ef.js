05b8a89d3df630f8531174d133fe98da6fbdbb83
Trausti Dagsson <traustid@gmail.com>
2020-11-09 10:40:07 +0000

    Teljari og breyting á texta um leikinn

---
+++ build/precache-manifest.25200a7283f601b1d3eaed79115306ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721df194366004c6414eff8665588a73",
+    "url": "/spagetti/index.html"
+  },
+  {
+    "revision": "65602f16f2bb89a61d47",
+    "url": "/spagetti/static/css/main.09c047b7.chunk.css"
+  },
+  {
+    "revision": "22164998fc94c5571be1",
+    "url": "/spagetti/static/js/2.5d374ef5.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/spagetti/static/js/2.5d374ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65602f16f2bb89a61d47",
+    "url": "/spagetti/static/js/main.4e7bb0fa.chunk.js"
+  },
+  {
+    "revision": "7b692bae73712f1359cf",
+    "url": "/spagetti/static/js/runtime-main.d0b3976f.js"
+  },
+  {
+    "revision": "6cc74d3a087b9a6593abf45b73a294e9",
+    "url": "/spagetti/static/media/game-logo.6cc74d3a.png"
+  },
+  {
+    "revision": "e0f7f8a88559a12b15633afd2382645d",
+    "url": "/spagetti/static/media/sam-logo.e0f7f8a8.png"
+  }
+]);
--- src/About.js
+++ src/About.js
@@ -13,19 +13,23 @@ export default function About() {
                 <div className="row">
                     <div className="col" style={{ marginTop: isMobile ? 30 : 100, marginLeft: isMobile ? 0 : 100, marginRight: isMobile ? 0 : 100}}>
                         <div>
-                            <h2>Markmið leiksins</h2>
+                            <h3>Markmið leiksins</h3>
                             <p>Svaraðu eins mörgum orðskýringum og þú getur þangað til tíminn rennur út með því að draga
                             orðskýringu í dálk sem samsvarar réttu orði.
                         </p>
                         </div>
-                        <div >
-                            <h2>Hvernig á að spila</h2>
-                            <p>Vinstra megin á síðunni birtist dálkur með orðum og hægra megin birtist dálkur með orðskýringum.
-                            Á milli þeirra birtist tómur dálkur. Dragðu orðskýringu yfir í miðju dálkinn í reitinn með sömu tölu
+						<div >
+                            <h3>Hvernig á að spila</h3>
+                            <p>Vinstra megin á síðunni birtist dálkur með orðum og hægra megin dálkur með orðskýringum.
+                            Á milli þeirra birtist tómur dálkur. Dragðu orðskýringu yfir í miðjudálkinn í reit með sömu tölu
                             og orðið sem skýringin á við til þess að vinna inn stig. Ef dregið er að vitlausu orði er ekki hægt
-                            að giska á skýringu að því orði aftur. Fimm orð og orðskýringar birtast í senn. Haldið er utan um rétt og röng svör og má sjá fjölda þeirra
-                            fyrir ofan dálkana. Þegar búið er að giska á öll fimm orðin birtast fimm ný orð þangað til tíminn rennur út.
+                            að giska á skýringu á því orði aftur. Fimm orð og orðskýringar birtast í senn. Haldið er utan um rétt og röng svör og má sjá fjölda þeirra
+                            fyrir ofan dálkana. Þegar giskað hefur verið á öll fimm orðin birtast fimm ný orð þangað til tíminn rennur út.
                         </p>
+                        </div>
+						<div >
+                            <h3>Um leikinn</h3>
+                            <p><strong>Spagettí</strong> er leikur sem Vinir Árnastofnunar létu útbúa fyrir styrk frá Málræktarsjóði. Í leiknum eru notuð orð sem finnast í Ritmálssafni Orðabókar Háskólans og Íslenskri nútímamálsorðabók. Leikurinn var forritaður af Andreu Ósk Sigurðardóttur.</p>
                         </div>
                         <div className="homeButton">
                             <a href="/" class="btn buttonGreen btn-lg">Aftur á forsíðu</a>
